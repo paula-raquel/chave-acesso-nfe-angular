5f7bdf0c783a18482e6fe2cf1d3a0c1150b79dde
Paula Raquel <kell.ef22@gmail.com>
2022-08-10 22:56:35 -0300

    Implementado o function com material de copiar clipboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { FormChaveComponent } from './component/form-chave/form-chave.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClipboardModule }  from '@angular/cdk/clipboard';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { FormChaveComponent } from './component/form-chave/form-chave.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
